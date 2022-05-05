d4dfa053a81fe2ed5695be10ed8183e57267ad09
marcuschj <haojie.chua@gmail.com>
2022-05-05 11:33:41 +0800

    Create/update profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); // middleware which checks token
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/', auth, async (req, res) => { // added middleware auth to protect route
+    try {
+        const user = await User.findById(req.user.id).select('--password'); // exclude password
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+}); 
+
+// @route   POST api/auth
+// @desc    Authenticate user & Get token
+// @access  Public
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (!user) { // error if no such user exists
+                // Just to maintain same error msg syntax as above
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password); // compare pw from req with user's password
+
+            if (!isMatch) {
+                // Just to maintain same error msg syntax as above
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+            
+            const payload = {
+                user: {
+                    id: user.id // mongodb uses _id, but mongoose abstracts it to id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), // secret
+                { expiresIn: 360000 },  // in seconds
+                (err, token) => {       // we either get err or token - handle accordingly
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+        
+    }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User'); 
 
 // @route   POST api/users
 // @desc    Register user
@@ -14,12 +19,61 @@ router.post('/',
         check('password', 'Please enter a password with 6 or more characters')
             .isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('User route');
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) { // should not be able to find existing user with same email
+                // Just to maintain same error msg syntax as above
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            const avatar = gravatar.url(email, {
+                s: '200', // size
+                r: 'pg',  // rating: PG
+                d: 'mm'   // default
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id // mongodb uses _id, but mongoose abstracts it to id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), // secret
+                { expiresIn: 360000 },  // in seconds
+                (err, token) => {       // we either get err or token - handle accordingly
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+        
     }
 );
 
